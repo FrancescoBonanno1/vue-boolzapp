99f16673142625fb1c8f107e34f99a3d54fce18a
Francesco Bonanno <francescosoapbonanno@gmail.com>
2023-11-22 12:10:43 +0100

    piccoli progressi, e sostituzione dell'array completo

--- main.js
+++ main.js
@@ -3,74 +3,180 @@ createApp({
 
     data() {
         return {
-            Contatti: [{
-                foto: `img/avatar_1.jpg`,
-                nome: `Michele`,
-                messaggi:{
-                    testo :["ciao, tutto bene?", "va bene, fammi sapere", "per caso hai preso la mia borsa dimensionale?"],
-                    inviato : true
+            contacts: [
+                {
+                    name: 'Michele',
+                    avatar: './img/avatar_1.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Hai portato a spasso il cane?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Ricordati di stendere i panni',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 16:15:22',
+                            message: 'Tutto fatto!',
+                            status: 'received'
+                        }
+                    ],
                 },
-                ora: `12.00`
-            },
-            {
-                foto: `img/avatar_2.jpg`,
-                nome: `Fabio`,
-                ultimomessaggio: ``,
-                ora: `12.00`
-            },
-            {
-                foto: `img/avatar_3.jpg`,
-                nome: `Samuele`,
-                ultimomessaggio: ``,
-                ora: `12.00`
-            },
+                {
+                    name: 'Fabio',
+                    avatar: './img/avatar_2.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '20/03/2020 16:30:00',
+                            message: 'Ciao come stai?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '20/03/2020 16:30:55',
+                            message: 'Bene grazie! Stasera ci vediamo?',
+                            status: 'received'
+                        },
+                        {
+                            date: '20/03/2020 16:35:00',
+                            message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'Samuele',
+                    avatar: './img/avatar_3.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '28/03/2020 10:10:40',
+                            message: 'La Marianna va in campagna',
+                            status: 'received'
+                        },
+                        {
+                            date: '28/03/2020 10:20:10',
+                            message: 'Sicuro di non aver sbagliato chat?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '28/03/2020 16:15:22',
+                            message: 'Ah scusa!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Alessandro B.',
+                    avatar: './img/avatar_4.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Lo sai che ha aperto una nuova pizzeria?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Si, ma preferirei andare al cinema',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Alessandro L.',
+                    avatar: './img/avatar_5.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ricordati di chiamare la nonna',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Va bene, stasera la sento',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Claudia',
+                    avatar: './img/avatar_6.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ciao Claudia, hai novità?',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Non ancora',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 15:51:00',
+                            message: 'Nessuna nuova, buona nuova',
+                            status: 'sent'
+                        }
+                    ],
+                },
+                {
+                    name: 'Federico',
+                    avatar: './img/avatar_7.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Fai gli auguri a Martina che è il suo compleanno!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'Grazie per avermelo ricordato, le scrivo subito!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                {
+                    name: 'Davide',
+                    avatar: './img/avatar_8.jpg',
+                    visible: true,
+                    messages: [
+                        {
+                            date: '10/01/2020 15:30:55',
+                            message: 'Ciao, andiamo a mangiare la pizza stasera?',
+                            status: 'received'
+                        },
+                        {
+                            date: '10/01/2020 15:50:00',
+                            message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
+                            status: 'sent'
+                        },
+                        {
+                            date: '10/01/2020 15:51:00',
+                            message: 'OK!!',
+                            status: 'received'
+                        }
+                    ],
+                },
+                
+            ]
 
-            {
-                foto: `img/avatar_4.jpg`,
-                nome: `Davide`,
-                ultimomessaggio: ``,
-                ora: `12.00`
-            },
 
-            {
-                foto: `img/avatar_5.jpg`,
-                nome: `alessandro B.`,
-                ultimomessaggio: ``,
-                ora: `12.00`
-            },
-
-            {
-                foto: `img/avatar_6.jpg`,
-                nome: `Alessandro L`,
-                ultimomessaggio: ``,
-                ora: `12.00`
-            },
-            {
-                foto: `img/avatar_7.jpg`,
-                nome: `Claudia`,
-                ultimomessaggio: ``,
-                ora: `12.00`
-            },
-            {
-                foto: `img/avatar_8.jpg`,
-                nome: `Federico`,
-                ultimomessaggio: ``,
-                ora: `12.00`
-            },
-            ],
-            id:[{
-                foto: `img/avatar_io.jpg`,
-                nome : `Sofia`,
-                messaggi :``
-            }],
 
         };
 
     },
 
     methods: {
-    
-        },
+
+    },
 
     mounted() {
 
